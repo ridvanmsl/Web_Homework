4f09313c3b286f0ee7cdf457c9dd2a72dce10403
ridvanmsl <93757575+ridvanmsl@users.noreply.github.com>
2022-12-18 04:16:33 +0300

    added about controller(working)

---
+++ Controllers/About.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+
+namespace WebAssignmentt.Controllers
+{
+    public class About : Controller
+    {
+        public IActionResult about()
+        {
+            return View("~/Views/Home/movies/about.cshtml");
+        }
+    }
+}
