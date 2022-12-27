98c5cc3a61d17ac9c1be50fe07c34bf148107efe
armanyayci <armanyayci@icloud.com>
2022-12-27 21:11:37 +0300

    jhgj

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAssignmentt.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
+
